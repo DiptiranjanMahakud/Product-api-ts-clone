d77384d1d17ff883dce0cacd4b52f4f41c41c134
DiptiranjanMahakud <diptiranjanmohakud304@gmail.com>
2024-01-24 18:56:11 +0530

    added

--- build/server.js
+++ build/server.js
@@ -10,6 +10,9 @@ const port = 3001;
 app.use(express.json());
 // Mount the ProductRoutes at the specified path
 app.use("/api", ProductRoutes);
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
 // Start the server and listen on the specified port
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
