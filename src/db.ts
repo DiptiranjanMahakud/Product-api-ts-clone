5282c36d88e3c1c99d2f2d6f4a918b3d3f9e58c7
DiptiranjanMahakud <diptiranjanmohakud304@gmail.com>
2024-01-25 17:32:35 +0530

    Added

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 const Pool=require("pg").Pool;
 
 var pool=new Pool({
-    user:"aws-postgres",
+    user:"dipti",
     host:"ls-1edb1b61210d847b3f66478fec067688142b4de6.cpmugeymehfj.ap-south-1.rds.amazonaws.com",
     database:"dbdipti",
     port:5432,
