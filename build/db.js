3e9aceab4cb32c8b5deb68e79380ef61d6a6d39c
DiptiranjanMahakud <diptiranjanmohakud304@gmail.com>
2024-01-25 16:41:36 +0530

    Added

--- build/db.js
+++ build/db.js
@@ -2,7 +2,7 @@
 const Pool = require("pg").Pool;
 var pool = new Pool({
     user: "aws-postgres",
-    host: "ls-4a43ddcbc4895ee92d5727f5d6e1635f9ed266c7.cpmugeymehfj.ap-south-1.rds.amazonaws.com",
+    host: "ls-1edb1b61210d847b3f66478fec067688142b4de6.cpmugeymehfj.ap-south-1.rds.amazonaws.com",
     database: "dbdipti",
     port: 5432,
     password: "dipti2002",
